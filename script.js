ebb5559401bb9007db8b3fd576a47ec413fdd191
Miles Debinski <miles.debinski@gmail.com>
2021-03-05 17:51:32 +0000

    update ui, fix body change

--- script.js
+++ script.js
@@ -68,18 +68,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
       if (cookie_1.style.visibility != 'hidden' && playerPosition[0] > 73 && playerPosition[0] < 85 && playerPosition[1] < -34 && playerPosition[1] > -43) {
         cookie_1.style.visibility = 'hidden';
-        // characterSpritesheet.style.backgroundImage = "url('/img/normal.png')"
         stepsCount > 180 ? stepsCount -= 180 : stepsCount = 0;
-        state.textContent = 'Normal'
+
       }
       // Cookie-2
       let cookie_2 = document.getElementById('cookie-2')
       if (cookie_2.style.visibility != 'hidden' && playerPosition[0] > -79 && playerPosition[0] < -67 && playerPosition[1] < 27 && playerPosition[1] > 16) {
         cookie_2.style.visibility = 'hidden';
-        // characterSpritesheet.style.backgroundImage = "url('/img/fat.png')"
         stepsCount > 320 ? stepsCount -= 320 : stepsCount = 0;
 
-        state.textContent = 'Fat'
       }
       console.log(stepsCount);
 
@@ -93,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
       if (stepsCount >= 160 && stepsCount < 320) {
         characterSpritesheet.style.backgroundImage = "url('/img/normal.png')"
         state.textContent = 'Normal'
-        // stepsCount = 0;
+
       }
       if (stepsCount < 160) {
         characterSpritesheet.style.backgroundImage = "url('/img/fat.png')"
